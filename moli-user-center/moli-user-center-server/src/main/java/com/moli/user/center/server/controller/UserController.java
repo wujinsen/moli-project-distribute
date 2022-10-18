06939e8c294ba3f7e58bcebb418c222a617945dd
wujinsen <2998714655@qq.com>
2022-10-18 19:44:40 +0800

    commit

--- moli-user-center/moli-user-center-server/src/main/java/com/moli/user/center/server/controller/UserController.java
+++ moli-user-center/moli-user-center-server/src/main/java/com/moli/user/center/server/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.moli.user.center.server.controller;
 
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -23,6 +24,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
